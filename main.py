fb3eb350c90d437361bb090b519544bf9977d3a8
memegauste <memegauste@gmail.com>
2023-12-13 11:18:04 +0100

    Initial changes for AnonimoweDiscoWyznania

---
+++ main.py
@@ -0,0 +1,54 @@
+"""Anonimowe disco wyznania"""
+import discord
+from discord.ext import commands
+from discord.ext.commands import DefaultHelpCommand
+from utils import read_json_file
+from utils import get_commit_version
+
+
+
+class CustomHelpCommand(DefaultHelpCommand):
+
+    def __init__(self, **options):
+        super().__init__(**options)
+        self.no_category = 'General'
+
+
+config = read_json_file('config.json')
+desc = 'AnonimoweDiscoWyznania.'
+intents = discord.Intents.default()
+intents.message_content = True
+activity = discord.Game('Made by NosApki')
+bot = commands.Bot(
+    intents=intents,
+    command_prefix='!',
+    activity=activity,
+    description=desc,
+    help_command=CustomHelpCommand(),
+)
+
+
+@bot.event
+async def on_ready():
+    """Print that bot logged in as."""
+    print(f'Logged in as {bot.user.name} | ID: {bot.user.id}!')
+
+
+@bot.command(brief='Shows user avatar')
+async def avatar(ctx):
+    if ctx.message.mentions:
+        mention = ctx.message.mentions[0]
+        await ctx.send(mention.avatar.url)
+
+
+@bot.command(brief='Estimates ping from bot server to discord')
+async def ping(ctx):
+    await ctx.send(f'Pong: {round(bot.latency, 7)}...')
+
+
+@bot.command(brief='Get actual running version of AnonimoweDiscoWyznania')
+async def version(ctx):
+    await ctx.send(f'{get_commit_version()}')
+
+
+bot.run(config.get('token'))
---
+++ utils.py
@@ -0,0 +1,26 @@
+"""Various utils made for AnonimoweDiscoWyznania."""
+import json
+import os
+import subprocess
+
+
+def read_json_file(path):
+    """Returns dict from json file."""
+    with open(path, 'r', encoding='utf-8') as f:
+        json_dict = json.load(f)
+    return json_dict
+
+
+def init_file_existence(path):
+    """Check file existence, if not, create it."""
+    if not os.path.exists(path):
+        with open(path, 'w', encoding='utf-8') as f:
+            json.dump({}, f, ensure_ascii=False, indent=4)
+
+
+
+def get_commit_version():
+    """Return SHORT commit number."""
+    return subprocess.check_output(
+        ['git', 'log', '--format="%H"', '-n 1'],
+    ).decode('ascii').replace('"', '')[:7]
