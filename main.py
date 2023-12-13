dc8db009a03ff5e9e5ccb504269413d7dec58969
memegauste <memegauste@gmail.com>
2023-12-13 13:42:50 +0100

    Add final functionality (finally?)

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import DefaultHelpCommand
-from utils import read_json_file
-from utils import get_commit_version
+
+from utils import get_commit_version, read_json_file
 
 
 class CustomHelpCommand(DefaultHelpCommand):
@@ -45,9 +45,18 @@ async def ping(ctx):
     await ctx.send(f'Pong: {round(bot.latency, 7)}...')
 
 
-@bot.command(brief='Get actual running version of AnonimoweDiscoWyznania')
-async def version(ctx):
+@bot.command(brief='Get actual running commit version of AnonimoweDiscoWyznania')
+async def commit_version(ctx):
     await ctx.send(f'{get_commit_version()}')
+
+
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+    designation_id = config.get('designation_id')
+    if not message.guild and designation_id:
+        channel = bot.get_channel(designation_id)
+        await channel.send(f'{message.content}')
 
 
 bot.run(config.get('token'))
