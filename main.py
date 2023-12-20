520f5ed7ae80bf301ac56734e120bd1e979a7826
PSzczepanski1996 <p.szczepanski996@gmail.com>
2023-12-20 21:32:30 +0100

    Add initial support for slash & modals

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """Anonimowe disco wyznania"""
 import discord
+from discord import app_commands
 from discord.ext import commands
 from discord.ext.commands import DefaultHelpCommand
 
@@ -18,65 +19,34 @@ desc = 'AnonimoweDiscoWyznania.'
 intents = discord.Intents.default()
 intents.message_content = True
 activity = discord.Game('Made by NosApki')
-bot = commands.Bot(
+client = discord.Client(
     intents=intents,
     command_prefix=config.get('cmd_prefix', '!'),
     activity=activity,
     description=desc,
     help_command=CustomHelpCommand(),
 )
+tree = app_commands.CommandTree(client)
 
 
-@bot.event
+@client.event
 async def on_ready():
     """Print that bot logged in as."""
-    print(f'Logged in as {bot.user.name} | ID: {bot.user.id}!')
+    print(f'Logged in as {client.user.name} | ID: {client.user.id}!')
 
 
-@bot.command(brief='Shows user avatar')
-async def avatar(ctx):
-    if ctx.message.mentions:
-        mention = ctx.message.mentions[0]
-        await ctx.send(mention.avatar.url)
-
-
-@bot.command(brief='Estimates ping from bot server to discord')
-async def ping(ctx):
-    await ctx.send(f'Pong: {round(bot.latency, 7)}...')
-
-
-@bot.command(brief='Get actual running commit version of AnonimoweDiscoWyznania')
-async def commit_version(ctx):
-    await ctx.send(f'{get_commit_version()}')
-
-
-@bot.command(brief='Wyślij anonimową wiadomość')
-async def anonim(ctx):
-    if not ctx.message.guild:
-        return
-    content = f'{ctx.message.content}'.removeprefix('!anonim ')
-    await ctx.message.delete()
-    await ctx.send(
-        f'```'  # noqa: W604
-        f'{content}'
-        f'```'
+class AnonymousModal(discord.ui.Modal, title='AnonimoweDiscoWyznania'):
+    designation = discord.ui.TextInput(
+        label='Wyznanie',
     )
 
-
-@bot.event
-async def on_message(message):
-    await bot.process_commands(message)
-    designation_id = config.get('designation_id')
-    if not message.content.startswith('!anonim'):
-        return
-    if not message.guild and designation_id:
-        channel = bot.get_channel(designation_id)
-        response = f'{message.content}'.removeprefix('!anonim ')
-        await channel.send(
-            f'```'  # noqa: W604
-            f'{response}'
-            f'```'
-        )
+    async def on_submit(self, interaction):
+        pass
 
 
-bot.run(config.get('token'))
+@tree.command(description='Wyślij anonimową wiadomość')
+async def anonim(interaction, msg: str):
+    interaction.response.send_modal(AnonymousModal())
+
+
+client.run(config.get('token'))
