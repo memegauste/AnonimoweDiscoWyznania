d8e8924344a3d7ab107710dd8f0e3226866c6acb
p_szczepanski996 <pszczepanski@webtechnika.pl>
2024-01-02 09:14:39 +0100

    More changes

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,3 +1,19 @@
+"""Admin file."""
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Local
+from .models import Designate
+
+
+@admin.site.register(Designate)
+class DesignateAdmin(admin.ModelAdmin):
+    """Designate admin file."""
+
+    list_display = [
+        'msg_id',
+        'approved',
+    ]
+    search_fields = [
+        'msg_id',
+    ]
